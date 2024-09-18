86fa3f8aca28521c003e6a8bb18a1332a63fa29c
heisonyee <heisonyee@126.com>
2024-09-18 15:05:39 +0800

    Add setter

--- model/schema.go
+++ model/schema.go
@@ -4,7 +4,7 @@ import (
 	bunx "github.com/blink-io/hyperbun"
 )
 
-type Schema[M any, C any] struct {
+type Schema[M Model, C Columns] struct {
 	PK      []string
 	Label   string
 	Alias   string
@@ -35,4 +35,12 @@ func (c Column) Safe() bunx.Safe {
 
 func (c Column) String() string {
 	return string(c)
+}
+
+type Model interface {
+	IsModel()
+}
+
+type Columns interface {
+	IsColumns()
 }
