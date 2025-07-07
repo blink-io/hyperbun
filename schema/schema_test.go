5adf84202d7eafaf28fb1fe4ca002f61722ec405
heisonyep <heisonyee@126.com>
2025-07-07 23:49:42 +0800

    eeddd

--- schema/schema_test.go
+++ schema/schema_test.go
@@ -18,28 +18,28 @@ func TestSchema_1(t *testing.T) {
 		Len  int
 	}
 	type MyTableColumns struct {
-		Name  Column
-		Level Column
+		Name  string
+		Level string
 	}
 	var myTableColumns = MyTableColumns{
 		Name:  "name",
 		Level: "level",
 	}
 
-	var schema = Schema[MyTable, MyTableColumns]{
-		PrimaryKeys: PrimaryKeys{"id1", "id2"},
-		Table:       "MyTable",
-		Label:       "MyTable",
+	var table = Table[MyTable, MyTableColumns]{
+		PrimaryKeys: []string{"id1", "id2"},
+		Schema:      "public",
+		Name:        "MyTable",
 		Model:       (*MyTable)(nil),
 		Alias:       "MyTable",
 		Columns:     myTableColumns,
 	}
 
-	fmt.Println(litter.Sdump(schema))
-	fmt.Printf("Type of Model: %#v\n", reflect.TypeOf(schema.Model).String())
+	fmt.Println(litter.Sdump(table))
+	fmt.Printf("Type of Model: %#v\n", reflect.TypeOf(table.Model).String())
 
-	var nn = schema.Columns.Level
-	var tt = schema.Table
+	var nn = table.Columns.Level
+	var tt = table.Name
 	fmt.Println(nn)
 	fmt.Println(tt)
 
@@ -47,12 +47,12 @@ func TestSchema_1(t *testing.T) {
 	var fmter2 = bschema.NewFormatter(mysqldialect.New())
 
 	var bb1 []byte
-	dd1, err1 := bun.Name(schema.Columns.Level).AppendQuery(fmter1, bb1)
+	dd1, err1 := bun.Name(table.Columns.Level).AppendQuery(fmter1, bb1)
 	require.NoError(t, err1)
 	fmt.Println(string(dd1))
 
 	var bb2 []byte
-	dd2, err2 := bun.Name(schema.Columns.Name).AppendQuery(fmter2, bb2)
+	dd2, err2 := bun.Name(table.Columns.Name).AppendQuery(fmter2, bb2)
 	require.NoError(t, err2)
 	fmt.Println(string(dd2))
 }
